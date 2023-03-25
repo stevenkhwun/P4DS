1e73ae58ffb0c6d81d8102e13181daf9a69025c5
Steven Wun <92497628+stevenkhwun@users.noreply.github.com>
2023-03-25 12:54:50 +0800

    Update some_module.py
    Add description

--- Module/some_module.py
+++ Module/some_module.py
@@ -1,4 +1,8 @@
 # some_module.py
+# This file is an example file used in the book
+# Python for Data Analysis (p.32)
+# This file is used in my Jupyter Notebook Chp01 in my GitHub repository P4DS
+# Last updated at 2023.03.25
 PI = 3.14159
 
 def f(x):
